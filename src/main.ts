2382fd391ecac4e3dcd73da14af46f9387f23ea6
Andres Ardila <andres.ardila@accenture.com>
2020-08-21 11:45:33 -0500

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   const options = new DocumentBuilder()
       .setTitle(process.env.APPLICATION_NAME)
